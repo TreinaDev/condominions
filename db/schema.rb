2aa5dd9585455f239b091b73ffb04a09d70b2f99
Marcella Aleo <cella.aleo@gmail.com>
2024-06-27 20:55:04 +0100

    Atualiza o schema.
    Co-authored-by: Danilo Ribeiro <danilo.ribeirodr01@gmail.com>
    Co-authored-by: Marcella Aleo <cella.aleo@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_200919) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_26_201555) do
   create_table "addresses", force: :cascade do |t|
     t.string "public_place"
     t.string "number"
