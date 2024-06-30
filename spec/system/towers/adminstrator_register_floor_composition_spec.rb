a61607e4726969551077ec6c59bbc432e489e948
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-06-30 15:45:52 -0300

    Cria teste de falha de cadastro de unidades dos andares da torre do condomínio.
    Co-authored-by: Lucas Lima <limalucks@hotmail.com>

--- spec/system/towers/adminstrator_register_floor_composition_spec.rb
+++ spec/system/towers/adminstrator_register_floor_composition_spec.rb
@@ -31,10 +31,33 @@ describe 'Adminstrator register floor type' do
     expect(current_path).to eq tower_path tower
     expect(page).to have_content 'Pavimento Tipo cadastrado com sucesso!'
     expect(page).to have_content 'Torre A'
-    expect(tower.floors.first.units.first.unit_type.description)
-      .to eq 'Apartamento de 2 quartos'
+    expect(tower.floors.first.units.first.unit_type.description).to eq 'Apartamento de 2 quartos'
+    expect(tower.floors.first.units.last.unit_type.description).to eq 'Apartamento de 1 quarto'
+  end
 
-    expect(tower.floors.first.units.last.unit_type.description)
-      .to eq 'Apartamento de 1 quarto'
+  it 'and fails if there is unselected unit types' do
+    tower = create :tower
+    tower.generate_floors
+
+    create :unit_type, description: 'Apartamento de 1 quarto'
+    create :unit_type, description: 'Apartamento de 2 quartos'
+
+    visit edit_floor_units_condo_tower_path(tower.condo, tower)
+
+    within '#unit-1' do
+      select 'Selecione o tipo de unidade', from: 'Tipo de Unidade'
+    end
+
+    within '#unit-2' do
+      select 'Selecione o tipo de unidade', from: 'Tipo de Unidade'
+    end
+
+    within '#unit-3' do
+      select 'Selecione o tipo de unidade', from: 'Tipo de Unidade'
+    end
+
+    click_on 'Atualizar Pavimento Tipo'
+
+    expect(page).to have_content 'Defina o tipo de unidade para todas as unidades!'
   end
 end
