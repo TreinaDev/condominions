7c054b8029f09ecc567c6ea3db64a5a13534876b
Marcella Aleo <cella.aleo@gmail.com>
2024-07-08 20:30:11 +0100

    Refatora testes das torres, aplica bootstrap em index de torres e corrige texto em show de condo.
    Co-authored-by: Vinícius Peruzzi <vinigperuzzi@hotmail.com>

--- spec/system/towers/administrator_view_condo_towers_spec.rb
+++ spec/system/towers/administrator_view_condo_towers_spec.rb
@@ -9,8 +9,9 @@ describe 'Administrator view list of towers' do
     expect(current_path).to eq new_manager_session_path
   end
 
-  it 'and see a list of condo´s towers from condo show' do
+  it 'and only sees the list of towers that belong to the condo' do
     condo = create(:condo)
+    condo2 = create(:condo)
     user = create :manager
     tower_a = build :tower, name: 'Torre A', condo_id: condo.id
     tower_a.generate_floors
@@ -18,6 +19,8 @@ describe 'Administrator view list of towers' do
     tower_b.generate_floors
     tower_c = build :tower, name: 'Torre C', condo_id: condo.id
     tower_c.generate_floors
+    tower_z = build :tower, name: 'Torre Z', condo_id: condo2.id
+    tower_z.generate_floors
 
     login_as user, scope: :manager
     visit condo_path(condo)
@@ -28,6 +31,7 @@ describe 'Administrator view list of towers' do
       expect(page).to have_link 'Torre A'
       expect(page).to have_link 'Torre B'
       expect(page).to have_link 'Torre C'
+      expect(page).not_to have_link 'Torre Z'
     end
   end
 
@@ -43,26 +47,6 @@ describe 'Administrator view list of towers' do
     click_on 'Torre A'
 
     expect(current_path).to eq tower_path(tower_a)
-  end
-
-  it 'and don´t see other condo´s towers' do
-    condo_a = create(:condo)
-    condo_b = create(:condo)
-    user = create :manager
-    tower_a = build :tower, name: 'Torre A', condo_id: condo_a.id
-    tower_a.generate_floors
-    tower_b = build :tower, name: 'Torre B', condo_id: condo_b.id
-    tower_b.generate_floors
-
-    login_as user, scope: :manager
-    visit condo_path(condo_a)
-    click_on 'Listar Torres'
-
-    expect(current_path).to eq condo_towers_path(condo_a)
-    within '.list' do
-      expect(page).to have_link 'Torre A'
-      expect(page).not_to have_link 'Torre B'
-    end
   end
 
   it 'resident can´t see link to list of towers' do
