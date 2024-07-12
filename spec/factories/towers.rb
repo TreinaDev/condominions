7b87d32dcdae7dd06377ab0925982de7755cafc2
Rafael Salgado <rafael.salgado31@gmail.com>
2024-07-12 20:16:28 -0300

    Ajusta sugestao no PR, arruma a factory para evitar erros
    Co-authored-by:  Marcella Aleo <cella.aleo@gmail.com>

--- spec/factories/towers.rb
+++ spec/factories/towers.rb
@@ -14,10 +14,10 @@ FactoryBot.define do
         unit_types = evaluator.unit_types
 
         tower.floors.each do |floor|
-          floor.units[0].update(unit_type: unit_types[0])
-          floor.units[1].update(unit_type: unit_types[1])
-          floor.units[2].update(unit_type: unit_types[0])
-          floor.units[3].update(unit_type: unit_types[1])
+          floor.units[0].update(unit_type: unit_types[0 % unit_types.size])
+          floor.units[1].update(unit_type: unit_types[1 % unit_types.size])
+          floor.units[2].update(unit_type: unit_types[2 % unit_types.size])
+          floor.units[3].update(unit_type: unit_types[3 % unit_types.size])
         end
         tower.complete!
       end
--- spec/requests/tower_spec.rb
+++ spec/requests/tower_spec.rb
@@ -26,6 +26,7 @@ describe 'POST /towers' do
                            units_per_floor: 4 } }
 
     expect(response).to redirect_to root_path
+    expect(Tower.last.name).not_to eq 'Torre do Rubinhos'
   end
 end
 
