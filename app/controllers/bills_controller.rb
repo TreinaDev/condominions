024af0c085626b988afadb986f6986554c4e227d
Vinícius <vinigperuzzi@hotmail.com>
2024-07-19 06:17:15 -0300

    Adiciona breadcrumbs para o index
    Co-authored-by: Lucas Oliveira <obxlucas@gmail.com>

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,6 +3,7 @@ class BillsController < ApplicationController
   before_action :unit_for_current_resident
   rescue_from Faraday::ConnectionFailed, with: :connection_refused
   before_action :request_open_bills_list
+  before_action :set_breadcrumbs_for_index, only: %i[index]
 
   def index; end
 
@@ -18,5 +19,9 @@ class BillsController < ApplicationController
 
   def connection_refused
     redirect_to root_path, alert: t('alerts.bill.lost_connection')
+  end
+
+  def set_breadcrumbs_for_index
+    add_breadcrumb I18n.t('breadcrumb.bill.index')
   end
 end
