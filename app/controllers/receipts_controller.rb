55d9fbcacd3fc3774b285ea5f336c1a23c64b8fc
Vinícius <vinigperuzzi@hotmail.com>
2024-07-21 07:05:09 -0300

    Define mensagem de erro para formulário vazio ou nos formatos não desejados
    Co-authored-by: Lucas Oliveira <obxlucas@gmail.com>
    Co-authored-by: Angelo Maia <request.angelo@gmail.com>

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,5 +1,7 @@
 class ReceiptsController < ApplicationController
   before_action :authenticate_resident!, only: %i[create new]
+  before_action :define_resident, only: %i[create new]
+  before_action :check_image_presence, only: :create
   before_action :set_breadcrumbs_for_action, only: :new
 
   def new
@@ -7,7 +9,7 @@ class ReceiptsController < ApplicationController
   end
 
   def create
-    current_resident.update(receipt: params[:image])
+    render 'new' unless @resident.update(receipt: params[:image])
     response = Bill.send_post_request(current_resident, params[:bill_id])
     message = Bill.render_message(response)
     redirect_to bills_path, flash: message
@@ -17,5 +19,17 @@ class ReceiptsController < ApplicationController
 
   def set_breadcrumbs_for_action
     add_breadcrumb I18n.t("breadcrumb.receipt.#{action_name}")
+  end
+
+  def define_resident
+    @resident = current_resident
+  end
+
+  def check_image_presence
+    @bill_id = params[:bill_id]
+    return if params[:image].present?
+
+    @resident.add_error
+    render 'new', status: :unprocessable_entity
   end
 end
