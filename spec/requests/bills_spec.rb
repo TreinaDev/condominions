ac071597a830447e26d6855ad24aa3cedeae09b7
Vinícius <vinigperuzzi@hotmail.com>
2024-07-21 05:38:03 -0300

    Implementa testes de requisição para new
    Co-authored-by: Lucas Oliveira <obxlucas@gmail.com>
    Co-authored-by: Angelo Maia <request.angelo@gmail.com>

--- spec/requests/bills_spec.rb
+++ spec/requests/bills_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Bills/receipts' do
+describe 'Bills/Receipts' do
   context 'GET /bills/{id}' do
     it 'must be authenticated as Resident to see (not authenticated)' do
       get bill_path 1
@@ -13,6 +13,23 @@ describe 'Bills/receipts' do
 
       login_as manager, scope: :manager
       get bill_path 1
+
+      expect(response).to redirect_to new_resident_session_path
+    end
+  end
+
+  context 'GET /bills/bill_id/receipts/new' do
+    it 'must be authenticated as Resident to see (not authenticated)' do
+      get new_bill_receipt_path 1
+
+      expect(response).to redirect_to new_resident_session_path
+    end
+
+    it 'must be authenticated as Resident to see (authenticated as super manager)' do
+      manager = create :manager, is_super: true
+
+      login_as manager, scope: :manager
+      get new_bill_receipt_path 1
 
       expect(response).to redirect_to new_resident_session_path
     end
