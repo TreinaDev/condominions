179e15a48cafa8d08ffd0c64a708e0356deb8064
Vinícius <vinigperuzzi@hotmail.com>
2024-07-21 05:49:06 -0300

    Implementa restes de requisição para index e create e adiciona breadcrumb
    Co-authored-by: Lucas Oliveira <obxlucas@gmail.com>
    Co-authored-by: Angelo Maia <request.angelo@gmail.com>

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,5 +1,6 @@
 class ReceiptsController < ApplicationController
   before_action :authenticate_resident!, only: %i[create new]
+  before_action :set_breadcrumbs_for_action, only: :new
 
   def new
     @bill_id = params[:bill_id]
@@ -10,5 +11,11 @@ class ReceiptsController < ApplicationController
     response = Bill.send_post_request(current_resident, params[:bill_id])
     message = Bill.render_message(response)
     redirect_to bills_path, flash: message
+  end
+
+  private
+
+  def set_breadcrumbs_for_action
+    add_breadcrumb I18n.t("breadcrumb.receipt.#{action_name}")
   end
 end
--- spec/requests/bills_spec.rb
+++ spec/requests/bills_spec.rb
@@ -34,4 +34,38 @@ describe 'Bills/Receipts' do
       expect(response).to redirect_to new_resident_session_path
     end
   end
+
+  context 'GET /bills' do
+    it 'must be authenticated as Resident to see (not authenticated)' do
+      get bills_path
+
+      expect(response).to redirect_to new_resident_session_path
+    end
+
+    it 'must be authenticated as Resident to see (authenticated as super manager)' do
+      manager = create :manager, is_super: true
+
+      login_as manager, scope: :manager
+      get bills_path
+
+      expect(response).to redirect_to new_resident_session_path
+    end
+  end
+
+  context 'POST /bills/{bill_id}/receipts' do
+    it 'must be authenticated as Resident to see (not authenticated)' do
+      post bill_receipts_path 1, params: { image: 'receipt.jpg', unid_id: 1 }
+
+      expect(response).to redirect_to new_resident_session_path
+    end
+
+    it 'must be authenticated as Resident to see (authenticated as super manager)' do
+      manager = create :manager, is_super: true
+
+      login_as manager, scope: :manager
+      post bill_receipts_path 1, params: { image: 'receipt.jpg', unid_id: 1 }
+
+      expect(response).to redirect_to new_resident_session_path
+    end
+  end
 end
