d865c0ae792eebcc89ec8321db410703046728de
Vinícius <vinigperuzzi@hotmail.com>
2024-07-22 02:44:33 -0300

    Testa a autorização do comprovante
    Co-authored-by: Lucas Oliveira <obxlucas@gmail.com>

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,9 +1,10 @@
 class BillsController < ApplicationController
   rescue_from Faraday::ConnectionFailed, with: :connection_refused
   before_action :authenticate_resident!, only: %i[index show]
-  before_action :unit_for_current_resident
+  before_action :unit_for_current_resident, only: %i[index show]
   before_action :request_open_bills_list, only: :index
   before_action :request_bill_details, only: :show
+  before_action :autorize_resident, only: :show
   before_action :set_breadcrumbs_for_action, only: %i[index show]
 
   def index; end
@@ -33,5 +34,9 @@ class BillsController < ApplicationController
 
   def set_breadcrumbs_for_action
     add_breadcrumb I18n.t("breadcrumb.bill.#{action_name}")
+  end
+
+  def autorize_resident
+    redirect_to root_path, alert: t('alerts.bill.not_autorized') unless @bill.unit_id == @unit.id
   end
 end
