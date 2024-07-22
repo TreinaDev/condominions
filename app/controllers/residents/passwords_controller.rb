f9f282757097ae25199bfe01b9f6e90013ef5664
Lucas Oliveira <obxlucas@gmail.com>
2024-07-21 22:23:13 -0300

    Redireciona para root caso token de redefinição de senha seja inválido
    Co-authored-by: Vinícius Peruzzi <vinigperuzzi@hotmail.com>

--- app/controllers/residents/passwords_controller.rb
+++ app/controllers/residents/passwords_controller.rb
@@ -1,8 +1,24 @@
 module Residents
   class PasswordsController < Devise::PasswordsController
-    before_action :logout_current_user, only: [:edit]
+    def edit
+      add_breadcrumb I18n.t('breadcrumb.resident.edit_password')
+      self.resource = find_resource_by_token
 
-    protected
+      return redirect_to root_path, alert: t('alerts.resident.invalid_token') unless valid_token?
+
+      logout_current_user
+      super
+    end
+
+    private
+
+    def find_resource_by_token
+      resource_class.with_reset_password_token(params[:reset_password_token])
+    end
+
+    def valid_token?
+      resource.present? && resource.reset_password_period_valid?
+    end
 
     def logout_current_user
       sign_out(current_manager || current_resident) if manager_signed_in? || resident_signed_in?
