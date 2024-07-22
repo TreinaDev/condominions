bf5aacb10f74ad81c8d04ed3e14ec807d4b3786b
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-07-22 00:57:37 -0300

    Gera cancelamento de cobrança na aplicação PagueAluguel ao cancelar uma reserva de área comum.
    Co-authored-by: Marcella Aleo <cella.aleo@gmail.com>

--- spec/system/common_area/reservation/resident_cancel_common_area_reservation_spec.rb
+++ spec/system/common_area/reservation/resident_cancel_common_area_reservation_spec.rb
@@ -5,11 +5,15 @@ describe 'Resident cancel common area reservation' do
     common_area = create :common_area
     resident = create :resident, :with_residence, condo: common_area.condo
 
+    patch_data = Rails.root.join('spec/support/json/common_areas/single_charge_cancelation.json').read
+    allow(Faraday).to receive(:patch).and_return(double('response', body: patch_data, success?: true, status: 200))
+
     travel_to '01/07/2024' do
       reservation = create :reservation,
                            common_area:,
                            resident:,
                            date: '05/07/2024',
+                           single_charge_id: 1,
                            status: :confirmed
 
       login_as resident, scope: :resident
@@ -34,7 +38,7 @@ describe 'Resident cancel common area reservation' do
     resident = create :resident, :with_residence, condo: common_area.condo
 
     travel_to '03/07/2024' do
-      create :reservation, common_area:, resident:, date: '03/07/2024', status: :confirmed
+      create :reservation, common_area:, resident:, date: '03/07/2024', single_charge_id: 1, status: :confirmed
     end
 
     travel_to '05/07/2024' do
@@ -45,6 +49,37 @@ describe 'Resident cancel common area reservation' do
         expect(page).to have_content 'Reservado'
         expect(page).not_to have_button 'Cancelar'
       end
+    end
+  end
+
+  it 'fail if the connection is lost with external application' do
+    common_area = create :common_area
+    resident = create :resident, :with_residence, condo: common_area.condo
+
+    travel_to '01/07/2024' do
+      reservation = create :reservation,
+                           common_area:,
+                           resident:,
+                           date: '05/07/2024',
+                           single_charge_id: 1,
+                           status: :confirmed
+
+      login_as resident, scope: :resident
+      visit common_area_path common_area
+
+      within('.table > tbody > tr:nth-child(1) > .wday-5') do
+        accept_confirm { click_on 'Cancelar' }
+        reservation.reload
+
+        expect(page).to have_content 'Reservado'
+        expect(page).to have_button 'Cancelar'
+        expect(page).not_to have_button 'Reservar'
+      end
+
+      expect(page).to have_content 'Conexão perdida com o servidor do PagueAluguel'
+      expect(page).not_to have_content 'Reserva cancelada com sucesso'
+      expect(current_path).to eq common_area_path common_area
+      expect(reservation.canceled?).to be false
     end
   end
 end
