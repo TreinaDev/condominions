a017a803748420074af0c2bc349cf709d83f3574
Rafael Salgado <rafael.salgado31@gmail.com>
2024-07-21 23:11:52 -0300

    Ajusta erros de timezone no banco de dados
    Co-authored-by: Vinícius <vinigperuzzi@hotmail.com>
    Co-authored-by: Rulian de Jesus Cruz <rulian.c@protonmail.ch>
    Co-authored-by: Lucas Oliveira <obxlucas@gmail.com>
    Co-authored-by: Marcella Aleo <cella.aleo@gmail.com>
    Co-authored-by: Danilo <danilo.ribeirodr01@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module Condominions
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.active_record.default_timezone = :local
     config.time_zone = "America/Sao_Paulo"
 
     config.api = config_for(:api)
